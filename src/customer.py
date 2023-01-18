b3cd57328928eafd3d792fc2ea100f50aa1278ef
Rebecca Bell <rebeccaromanobell@gmail.com>
2023-01-18 17:33:31 +0000

    updates

--- src/customer.py
+++ src/customer.py
@@ -8,8 +8,8 @@ class Customer:
     def buy_a_drink(self, price):
         self.wallet -= price
 
-    def check_drunkenness(self,drunkenness):
-        return drunkenness <= 10
 
     def increase_drunkenness(self, drink):
-        self.drunkenness += int(drink.alcohol_level)
+        self.drunkenness += drink
+
+
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -8,10 +8,7 @@ class TestCustomer(unittest.TestCase):
         self.customer = Customer("Rupert", 10, 17)
         self.drink = Drink("Tennents", 2.50, 2)
 
-    def test_check_drunkenness(self):
-        # self.customer.customer_drunkenness(self.customer.drunkenness)
-        self.assertEqual(10, self.customer.customer_drunkenness(self.customer.drunkenness))
 
-    # def test_increase_drunkenness(self):
-    #     self.customer.increase_drunkenness(self.drink.name)
-    #     self.assertEqual(2, self.customer.increase_drunkenness(self.drink.alcohol_level))
+    def test_increase_drunkenness(self):
+        self.customer.increase_drunkenness(self.drink.alcohol_level)
+        self.assertEqual(2, self.customer.drunkenness)
